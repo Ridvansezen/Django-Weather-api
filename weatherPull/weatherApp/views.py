47aa1ffb28b1cbc7facf171cbb27965736001a26
Ridvansezen <sezenridvan309@gmail.com>
2023-08-27 15:59:00 +0300

    commit

--- weatherPull/weatherApp/views.py
+++ weatherPull/weatherApp/views.py
@@ -2,24 +2,44 @@ import requests
 from django.shortcuts import render
 
 def weather(request):
+    error_message = None
     api_key = 'ec415d880bf53898d7cea4e067bbfc15'
-    city = 'Mugla'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    city = request.POST.get('city', '')
 
-    response = requests.get(url)
-    data = response.json()
+    if city:
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
 
-    temperature = int(data['main']['temp'])
-    weather_description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
+        response = requests.get(url)
+        data = response.json()
 
-    city_name = city
+        if response.status_code == 200:
+            temperature = int(data['main']['temp'])
+            weather_description = data['weather'][0]['description']
+            icon = data['weather'][0]['icon']
 
-    context = {
-        'temperature': temperature,
-        'weather_description': weather_description,
-        'icon': icon,
-        'city_name' : city_name,
-    }
+            city_name = city
 
-    return render(request, 'weather.html', context)
+            context = {
+                'temperature': temperature,
+                'weather_description': weather_description,
+                'icon': icon,
+                'city_name': city_name,
+            }
+
+            turkish_translations = {
+                "broken clouds": "parçalı bulutlu",
+                "clear sky": "açık hava",
+                "overcast clouds" : "çok bulutlu",
+                "few clouds":"az bulutlu",
+                # Diğer hava durumu çevirilerini buraya ekleyin
+            }
+            if weather_description in turkish_translations:
+                context['weather_description'] = turkish_translations[weather_description]
+
+            return render(request, 'weather.html', context)
+        else:
+            error_message = "Belirtilen şehir bulunamadı."
+    else:
+        error_message = "Lütfen bir şehir adı girin."
+
+    return render(request, 'weather.html', {'error_message': error_message})
